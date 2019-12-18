d59b6d358ff5e4020f483194a973604b213e8c65
svelezg <svelez.velezgarcia@gmail.com>
2019-12-18 15:44:49 -0500

    Correction os places cities

--- models/state.py
+++ models/state.py
@@ -14,3 +14,14 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
+
+    if os.environ['HBNB_TYPE_STORAGE'] != 'db':
+        @property
+        def cities(self):
+            '''FileStorage relationship between State and City '''
+            cities = storage.all(City)
+            cities_relation = []
+            for city in cities.values():
+                if city.state_id == self.id:
+                    cities_relation = cities_relation.append(city)
+            return cities_relation
