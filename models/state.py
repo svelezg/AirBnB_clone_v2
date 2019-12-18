1117ef103cf384b9c3f405bcad4ea0cd776f2aae
Cesar Velez <cavb.28@gmail.com>
2019-12-18 09:57:33 -0500

    fixing errors in: db_storage and state

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -11,3 +11,8 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship(
+    "City",
+    backref="state",
+     cascade="all, delete-orphan"
+    )
