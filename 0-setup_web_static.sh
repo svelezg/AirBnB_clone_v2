28d6b40e11b69a1e9c3bffb35a22970472ccfb2e
svelezg <svelez.velezgarcia@gmail.com>
2020-01-03 15:10:06 -0500

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,16 +3,14 @@
 # Install nginx on server
 sudo apt-get -y update
 sudo apt-get -y install nginx
-# Create the folder /data/ if it doesn’t already exist
-sudo mkdir -p /data/
-# Create the folder /data/web_static/ if it doesn’t already exist
-sudo mkdir -p /data/web_static/
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-sudo mkdir -p /data/web_static/releases/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-sudo mkdir -p /data/web_static/shared/
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/test if it doesn’t already exist
+if [ ! -d /data/web_static/releases/test/ ]; then
 sudo mkdir -p /data/web_static/releases/test/
+fi;
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+if [ ! -d /data/web_static/shared/ ]; then
+sudo mkdir -p /data/web_static/shared/
+fi;
 # Create a fake HTML file /data/web_static/releases/test/index.html
 # (with simple content, to test your Nginx configuration)
 sudo touch /data/web_static/releases/test/index.html
@@ -25,10 +23,10 @@ sudo echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 # If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sfn /data/web_static/releases/test /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
 # This should be recursive; everything inside should be created/owned by this user/group.
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown ubuntu. -R /data
 # Update the Nginx configuration to serve the content of /data/web_static/current/
 # to hbnb_static (ex: https://mydomainname.tech/hbnb_static).
 sudo sed -i '20i location /hbnb_static/ {\nalias /data/web_static/current/;\nautoindex off;\n}' /etc/nginx/sites-available/default
